876e770bad4717d1f8323ea509968c05aea5c306
eric barrs <yellastar42@yahoo.com>
2020-06-26 17:44:08 -0500

    update

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -2,9 +2,7 @@ const fetch = require("node-fetch");
 
 module.exports.weatherData = async (req, res) => {
 	try {
-		const url = `http://api.weatherstack.com/forecast?access_key=${
-			process.env.ACCESS_KEY || "23c611c5efe997e520072b2cfc84a332"
-		}&query=${req.body.zipcode}`;
+		const url = `http://api.weatherstack.com/forecast?access_key=${process.env.ACCESS_KEY}&query=${req.body.zipcode}`;
 		const result = await fetch(url);
 		const data = await result.json();
 		res.json(data);

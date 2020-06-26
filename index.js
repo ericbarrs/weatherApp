c8da58ab9414872d170cbf2a5fd944121eb3b673
eric barrs <yellastar42@yahoo.com>
2020-06-26 15:53:41 -0500

    update

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ mongoose
 
 app.use(express.static("dashboard/build"));
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 

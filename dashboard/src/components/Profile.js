317007afdc35aa2161ec595c90810f695c2387bc
eric barrs <yellastar42@yahoo.com>
2020-06-28 17:08:10 -0500

    update profile

--- dashboard/src/components/Profile.js
+++ dashboard/src/components/Profile.js
@@ -42,119 +42,123 @@ class Profile extends React.Component {
 	render() {
 		return (
 			<div className="Profile">
-				<div className="Register Home">
-					<h1 className="heading">Profile</h1>
-					<Container fluid="sm">
-						<Row className="justify-content-md-center">
-							<Col xs lg="5">
-								<Form>
-									<Form.Group controlId="formBasicEmail">
-										<Form.Label>Email address</Form.Label>
-										<Form.Control
-											type="email"
-											placeholder="Enter email"
-											name="email"
-											value={this.state.email}
-											isInvalid={this.isItValid("email")}
-											onChange={(e) => this.updateHandler(e)}
-										/>
-									</Form.Group>
-									<Form.Group>
-										<Form.Label>First Name</Form.Label>
-										<Form.Control
-											type="input"
-											placeholder="First Name"
-											name="firstName"
-											value={this.state.firstName}
-											isInvalid={this.isItValid("firstName")}
-											onChange={(e) => this.updateHandler(e)}
-										/>
-									</Form.Group>
-									<Form.Group>
-										<Form.Label>Last Name</Form.Label>
-										<Form.Control
-											type="input"
-											placeholder="Last Name"
-											name="lastName"
-											value={this.state.lastName}
-											isInvalid={this.isItValid("lastName")}
-											onChange={(e) => this.updateHandler(e)}
-										/>
-									</Form.Group>
-									<Form.Group>
-										<Form.Label>City</Form.Label>
-										<Form.Control
-											type="input"
-											placeholder="City"
-											name="city"
-											value={this.state.city}
-											isInvalid={this.isItValid("city")}
-											onChange={(e) => this.updateHandler(e)}
-										/>
-									</Form.Group>
-									<Form.Group>
-										<Form.Label>State</Form.Label>
-										<Form.Control
-											type="input"
-											placeholder="State"
-											name="state"
-											value={this.state.state}
-											isInvalid={this.isItValid("state")}
-											onChange={(e) => this.updateHandler(e)}
-										/>
-									</Form.Group>
-									<Form.Group>
-										<Form.Label>Zipcode</Form.Label>
-										<Form.Control
-											type="text"
-											maxLength="5"
-											placeholder="Zipcode"
-											name="zipcode"
-											value={this.state.zipcode}
-											isInvalid={this.isItValid("zipcode")}
-											onChange={(e) => this.updateHandler(e)}
-										/>
-									</Form.Group>
-									<Form.Group controlId="formBasicPassword">
-										<Form.Label>Password</Form.Label>
-										<Form.Control
-											type="password"
-											placeholder="Password"
-											name="password"
-											isInvalid={this.isItValid("password")}
-											onChange={(e) => this.updateHandler(e)}
-										/>
-									</Form.Group>
-									<Form.Group controlId="verifyPassword">
-										<Form.Label>Verify Password</Form.Label>
-										<Form.Control
-											type="password"
-											name="password2"
-											placeholder="Verify Password"
-											isInvalid={this.isItValid("password")}
-											onChange={(e) => this.updateHandler(e)}
-										/>
-									</Form.Group>
-									<Button
-										variant="primary"
-										onClick={() => {
-											this.props.saveProfile(this.props.user.token, this.state);
-										}}
-									>
-										Save
-									</Button>
-								</Form>
-							</Col>
-						</Row>
-					</Container>
-					<div className="error">
-						{this.AlertMessage("email")}
-						{this.AlertMessage("firstName")}
-						{this.AlertMessage("lastName")}
-						{this.AlertMessage("city")}
-						{this.AlertMessage("state")}
-						{this.AlertMessage("zipcode")}
-						{this.AlertMessage("password")}
+				<div className="Register">
+					<div className="Home">
+						<h1 className="heading">Profile</h1>
+						<Container fluid="sm">
+							<Row className="justify-content-md-center">
+								<Col xs lg="5">
+									<Form>
+										<Form.Group controlId="formBasicEmail">
+											<Form.Label>Email address</Form.Label>
+											<Form.Control
+												type="email"
+												placeholder="Enter email"
+												name="email"
+												value={this.state.email}
+												isInvalid={this.isItValid("email")}
+												onChange={(e) => this.updateHandler(e)}
+											/>
+										</Form.Group>
+										{this.AlertMessage("email")}
+										<Form.Group>
+											<Form.Label>First Name</Form.Label>
+											<Form.Control
+												type="input"
+												placeholder="First Name"
+												name="firstName"
+												value={this.state.firstName}
+												isInvalid={this.isItValid("firstName")}
+												onChange={(e) => this.updateHandler(e)}
+											/>
+											{this.AlertMessage("firstName")}
+										</Form.Group>
+										<Form.Group>
+											<Form.Label>Last Name</Form.Label>
+											<Form.Control
+												type="input"
+												placeholder="Last Name"
+												name="lastName"
+												value={this.state.lastName}
+												isInvalid={this.isItValid("lastName")}
+												onChange={(e) => this.updateHandler(e)}
+											/>
+											{this.AlertMessage("lastName")}
+										</Form.Group>
+										<Form.Group>
+											<Form.Label>City</Form.Label>
+											<Form.Control
+												type="input"
+												placeholder="City"
+												name="city"
+												value={this.state.city}
+												isInvalid={this.isItValid("city")}
+												onChange={(e) => this.updateHandler(e)}
+											/>
+											{this.AlertMessage("city")}
+										</Form.Group>
+										<Form.Group>
+											<Form.Label>State</Form.Label>
+											<Form.Control
+												type="input"
+												placeholder="State"
+												name="state"
+												value={this.state.state}
+												isInvalid={this.isItValid("state")}
+												onChange={(e) => this.updateHandler(e)}
+											/>
+
+											{this.AlertMessage("state")}
+										</Form.Group>
+										<Form.Group>
+											<Form.Label>Zipcode</Form.Label>
+											<Form.Control
+												type="text"
+												maxLength="5"
+												placeholder="Zipcode"
+												name="zipcode"
+												value={this.state.zipcode}
+												isInvalid={this.isItValid("zipcode")}
+												onChange={(e) => this.updateHandler(e)}
+											/>
+											{this.AlertMessage("zipcode")}
+										</Form.Group>
+										<Form.Group controlId="formBasicPassword">
+											<Form.Label>Password</Form.Label>
+											<Form.Control
+												type="password"
+												placeholder="Password"
+												name="password"
+												isInvalid={this.isItValid("password")}
+												onChange={(e) => this.updateHandler(e)}
+											/>
+										</Form.Group>
+										<Form.Group controlId="verifyPassword">
+											<Form.Label>Verify Password</Form.Label>
+											<Form.Control
+												type="password"
+												name="password2"
+												placeholder="Verify Password"
+												isInvalid={this.isItValid("password")}
+												onChange={(e) => this.updateHandler(e)}
+											/>
+											{this.AlertMessage("password")}
+										</Form.Group>
+										<Button
+											variant="primary"
+											onClick={() => {
+												this.props.saveProfile(
+													this.props.user.token,
+													this.state
+												);
+											}}
+										>
+											Save
+										</Button>
+									</Form>
+								</Col>
+							</Row>
+						</Container>
 					</div>
 				</div>
 			</div>
